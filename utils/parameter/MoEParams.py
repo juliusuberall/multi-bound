3a6fe83693de4bb94d2b9494c92970fa1d981477
Julius Uberall <juliusueberall@gmail.com>
2025-04-10 00:20:56 +0100

    Add MoE parameter struct with 4 experts

--- utils/parameter/MoEParams.py
+++ utils/parameter/MoEParams.py
@@ -19,7 +19,20 @@ class MoEParams2E(MoEParams):
     expert1: list = None
     expert2: list = None
 
+class MoEParams4E(MoEParams):
+    """
+    <summary>
+        MoE paramater struct for 1 gate and 4 expert networks.
+    </summary>
+    """
+    gate: list = None
+    expert1: list = None
+    expert2: list = None
+    expert3: list = None
+    expert4: list = None
+
 # MoE parameter registry to identify which struct layout to use
 moe_params_registry = {
-    2 : MoEParams2E
+    2 : MoEParams2E,
+    4 : MoEParams4E
 }
